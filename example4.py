4723738a04a065bacfeffa5d0b429bea92545e69
kantmanav <kantmanav@gmail.com>
2026-03-06 10:40:41 -0800

    Add examples with free variables and free variables with bound variables

---
+++ example4.py
@@ -0,0 +1,15 @@
+from io import StringIO
+
+from pysmt.smtlib.parser import get_formula
+from pysmt.shortcuts import is_sat
+from pysmt.logics import LIA
+
+smtlib_content = r"""
+(declare-const x Nat)
+(assert (>= x 0))
+"""
+
+formula = get_formula(StringIO(smtlib_content))
+print("Formula: " + str(formula))
+sat = is_sat(formula, solver_name="cvc5", logic=LIA)
+print("Sat: " + str(sat))
---
+++ example7.py
@@ -0,0 +1,15 @@
+from io import StringIO
+
+from pysmt.smtlib.parser import get_formula
+from pysmt.shortcuts import is_sat
+from pysmt.logics import LIA
+
+smtlib_content = r"""
+(declare-const y Nat)
+(assert (forall ((x Nat)) (> y x)))
+"""
+
+formula = get_formula(StringIO(smtlib_content))
+print("Formula: " + str(formula))
+sat = is_sat(formula, solver_name="cvc5", logic=LIA)
+print("Sat: " + str(sat))
